aa6a5db30e146932c635ed65e6830471b2848823
bacondrake <frank.pavey@gmail.com>
2014-10-02 08:49:48 +1000

    Added some test specs

--- app/models/ntcmarshal.rb
+++ app/models/ntcmarshal.rb
@@ -2,4 +2,13 @@ class Ntcmarshal < ActiveRecord::Base
   has_and_belongs_to_many :exams
   validates_presence_of :name
   validates_uniqueness_of :name
+
+  # CSV for marshal name and exam count only
+  @marshals.each do |m|
+    File.open('marshals.csv', 'a') do |csv|
+      @row = [m.name, m.exams.count]
+    csv.puts @row.join(',')
+    end
+  end
+
 end
