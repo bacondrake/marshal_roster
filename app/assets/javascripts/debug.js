44676634438ccc857e223ee18597d84e715dfbca
bacondrake <frank.pavey@gmail.com>
2015-01-15 16:14:54 +1100

    Cleaned up /assets/* files

--- app/assets/javascripts/debug.js
+++ app/assets/javascripts/debug.js
@@ -1,3 +1,6 @@
+// Closes closest .debug class with a slideUp.
+// Used to remove debug information in development mode.
+
 $(document).ready(function() {
   $('.debug').on('click', function () {
     $(this).slideUp();
