086edea36c974b2574788ff961dba4378871f515
bacondrake <frank.pavey@gmail.com>
2015-01-16 11:49:48 +1100

    Only admins can sign up new users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   before_action :logged_in_user, only: [:edit, :update, :show, :destroy]
   before_action :correct_user, only: [:edit, :update, :show]
-  # before_action :admin_user, only: :destroy
+  before_action :admin_user, only: [:destroy, :new]
 
   def new
     @user = User.new
@@ -43,7 +43,7 @@ class UsersController < ApplicationController
   def destroy
     User.find(params[:id]).destroy
     flash[:success] = "Your profile has been deleted"
-    redirect_to signup_path
+    redirect_to signup_admin_path
   end
 
   private
@@ -69,7 +69,10 @@ class UsersController < ApplicationController
 
     # Use when needing admin user.
       # Confirms an admin user.
-      # def admin_user
-      #   redirect_to(root_url) unless current_user.admin?
-      # end
+      def admin_user
+        unless current_user.admin?
+          redirect_to(root_url)
+          flash.now[:danger] = "You do not have permission to view that page"
+        end
+      end
 end
